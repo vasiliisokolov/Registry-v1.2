ad524e6e07ea7cd2e8b4e57d2b3076adc5da7cd6
VasiliiSokolov <vasilysokolov@mail.ru>
2023-02-01 19:33:25 +0300

    Добавьте файлы проекта.

---
+++ Registry v1.2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+std::string find_patient(std::map<std::string,int>&);
+
+int main()
+{
+    std::string input;
+    bool working = true;
+    int patientNumber = 0;
+    std::map<std::string,int> list;
+    std::cout << "\tRegistry!" << std::endl;
+    while (working)
+    {
+        std::cin >> input;
+        if (input == "Exit")
+        {
+            working = false;
+        }
+        else if (input == "Next")
+        {
+            if (list.begin() == list.end())
+            {
+                std::cerr << "Error! The List is empty!" << std::endl;
+            }
+            else
+            {
+                std::cout << find_patient(list) << std::endl;
+            }
+        }
+        else
+        {
+            patientNumber++;
+            list.insert(std::pair<std::string,int>(input, patientNumber));
+
+        }
+    }
+
+}
+
+std::string find_patient(std::map<std::string, int>& list)
+{
+    std::map<std::string,int>::iterator it = list.begin();
+    std::string result = it->first;
+    list.erase(result);
+    return result;
+       
+}
